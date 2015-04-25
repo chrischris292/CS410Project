e964368b27bb53724f88329c6ae56c0e0cf945ff
chrischris292 <cbchan2@illinois.edu>
2015-04-25 18:55:30 -0500

    sentiment finished

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var port = process.env.PORT || 3000;
 
 var request = require('request');
 var cheerio = require('cheerio');
+var google = require('google')
+var sentiment = require('sentiment');
+var playerSentiment = [];
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -14,17 +17,14 @@ server.listen(port, function () {
 app.use(express.static(__dirname + '/public'));
 main();
 
+
 function main(){
-	webScrapePlayers();
+	webScrapePlayers2012(); // -> Google queries
+
 }
 
-
-
-
-
-
-
-function webScrapePlayers(){
+function webScrapePlayers2012(){
+	player = []
 	url = "http://www.nba.com/history/2012-draft-history/index.html"
     request(url, function(error, response, html){
 
@@ -42,8 +42,64 @@ function webScrapePlayers(){
             var playerName;
             var json = { title : "", release : "", rating : ""};
 			$('.cnnIERowAltBG').each(function() {
-			    console.log($(this).text().split("   ")[2]);
+			    player.push($(this).text().split("   ")[2]);
+			   	playerSentiment[$(this).text().split("   ")[2]] = [];
+			});
+			//because of async must call function after this:
+			googleQueryWrapper(player)
+		}
+    })
+}
+//no idea why this doesnt work
+function webScrapePlayers2014(){
+	url = "http://www.nba.com/draft/2014/draftboard.html"
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(html);
+            // Finally, we'll define the variables we're going to capture
+            var title, release, rating;
+            var playerRow;
+            var playerName;
+            var json = { title : "", release : "", rating : ""};
+			$('.pickDesc').each(function() {
+				console.log("hi")
+			    console.log($(this));
 			});
 		}
     })
+}
+
+function googleQueryWrapper(player){
+	for(i in player){
+		googleQuery(player[i])
+	}
+}
+function googleQuery(playerName){
+	google.resultsPerPage = 10
+	var nextCounter = 0
+
+	google(playerName + 'draft scouting report', function (err, next, links){
+  if (err) console.error(err)
+
+  for (var i = 0; i < links.length; ++i) {
+    console.log(links[i].title) // link.href is an alias for link.link
+    console.log(links[i].description + "\n")
+    article = links[i].title + " " + links[i].description;
+    var result = sentiment(article);
+    //console.log(result.score)
+    playerSentiment[playerName].push(sentiment(article))
+
+  }
+
+  if (nextCounter < 4) {
+    nextCounter += 1
+    if (next) next()
+  }
+})
+
 }
