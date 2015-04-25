6a0344338d2ec6bd6b25557f75568b484a013397
chrischris292 <cbchan2@illinois.edu>
2015-04-25 18:17:35 -0500

    2012 works now

--- index.js
+++ index.js
@@ -4,10 +4,46 @@ var app = express();
 var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
 
+var request = require('request');
+var cheerio = require('cheerio');
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
 app.use(express.static(__dirname + '/public'));
+main();
 
+function main(){
+	webScrapePlayers();
+}
+
+
+
+
+
+
+
+function webScrapePlayers(){
+	url = "http://www.nba.com/history/2012-draft-history/index.html"
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(html);
+
+            // Finally, we'll define the variables we're going to capture
+
+            var title, release, rating;
+            var playerRow;
+            var playerName;
+            var json = { title : "", release : "", rating : ""};
+			$('.cnnIERowAltBG').each(function() {
+			    console.log($(this).text().split("   ")[2]);
+			});
+		}
+    })
+}
